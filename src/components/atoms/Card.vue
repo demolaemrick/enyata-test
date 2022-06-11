95f1e394c13cf024ca95b179d3ad00acb1c1f81b
Ademola Akindotuni <emricky17@gmail.com>
2022-06-11 20:24:14 +0100

    Implement table component

---
+++ src/components/atoms/Card.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card">
+    <div class="card__content">
+      <div class="card__content__info">
+        <span>{{title}}</span>
+        <span>200</span>
+        <p>20 More than than yesterday</p>
+      </div>
+      <div class="card__icon" :style="{background: iconColor}"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["title", "iconColor"]
+};
+</script>
+<style>
+.card {
+  background: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  min-width: 208px;
+  padding: 24px 15px 17px;
+}
+.card__content {
+  display: flex;
+  justify-content: space-between;
+}
+.card__content__info {
+  display: flex;
+  flex-direction: column;
+  gap: 23px;
+}
+.card__content__info span {
+  color: #303b54;
+  font-weight: 700;
+}
+.card__content__info p {
+  color: #00992b;
+  font-size: 9px;
+  line-height: 24px;
+}
+.card__icon {
+  background: #a9c1ff;
+  width: 1.69rem;
+  height: 1.63rem;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/organisms/Table.vue
@@ -0,0 +1,74 @@
+<template>
+  <table>
+    <caption class="table-title">
+      Starships
+    </caption>
+
+    <tr>
+      <th>
+        <input type="checkbox" />
+      </th>
+      <th v-for="(header, i) in headers" :key="i">{{ header.name }}</th>
+    </tr>
+    <tr v-for="(item, i) in data" :key="i">
+      <td>
+        <input type="checkbox" />
+      </td>
+      <td v-for="(header, i) in headers" :key="i" @click="navigate">
+        {{ item[header.key] }}
+      </td>
+    </tr>
+  </table>
+</template>
+<script>
+export default {
+  props: ["headers", "data", "page"],
+  methods: {
+    navigate() {
+      this.$router.push(`/${this.page}/1`);
+    },
+  },
+};
+</script>
+<style scoped>
+.table-title {
+  color: #a4a7b7;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: left;
+  margin-bottom: 31px;
+}
+table {
+  width: 100%;
+  border: 1px solid rgba(164, 167, 183, 0.4);
+  border-radius: 4px;
+  border-spacing: 0;
+}
+th {
+  font-weight: 500;
+  color: #a4a7b7;
+  font-size: 16px;
+  line-height: 24px;
+}
+tr {
+  -webkit-box-shadow: 0px 1px 0px 0px rgba(229, 229, 229, 1);
+  -moz-box-shadow: 0px 1px 0px 0px rgba(229, 229, 229, 1);
+  box-shadow: 0px 1px 0px 0px rgba(229, 229, 229, 1);
+}
+tr:hover:not(:first-of-type) {
+  background: #ffffff;
+  box-shadow: 0px 0px 30px rgba(13, 47, 161, 0.07);
+  border-radius: 4px;
+}
+
+th,
+td {
+  padding: 30px;
+}
+td {
+  color: #303b54;
+  line-height: 24px;
+  text-align: center;
+  font-weight: 500;
+}
+</style>
